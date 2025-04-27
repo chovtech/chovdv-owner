cfc85085414dfc7a2841782cab7b3f6cb3217135
Abba <sunnyogaje@gmail.com>
2025-04-27 21:32:00 +0100

    Affiliate features

---
+++ affiliate/transactions/index.php
@@ -0,0 +1,187 @@
+<?php
+    include('../../controller/session/session-checker-franchise.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="EduMESS" />
+        <meta name="description"
+            content="EduMESS (Education Management and E-Learning Software Solution)
+            is a leading school management, automation and elearning solution. Since its
+            launch, EduMESS has grown to become a leader. Our clients say that the simplicity
+            of our interface, ease of use, cost effectiveness and excellent customer care is
+            the reason they prefare EduMESS. We have watched schools move from softwares they
+            are using to EduMESS." />
+        <meta name="keywords"
+            content="Best, School, Management, Best School, Best School Management,
+            Best School Management Software, Free School Management Software, Portal,
+            School Owner, Group of School Owner, Consultants, Brand Promoters | School Portal Generator ">
+            <title>EduMESS | Affiliates</title>
+            <!-- FAVICON AND TOUCH ICONS -->
+        <link rel="shortcut icon" href="../../assets/images/website_images/favicon.png" type="image/x-icon">
+        <link rel="icon" href="../../assets/images/website_images/favicon.png" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="152x152" href="../../assets/images/website_images/favicon.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="../../assets/images/website_images/favicon.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="../../assets/images/website_images/favicon.png">
+        <link rel="apple-touch-icon" href="../../assets/images/website_images/favicon.png">
+        <link rel="icon" href="../../assets/images/website_images/favicon.png" type="image/x-icon">
+
+        <link href="../../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+
+        <link href="../../notify/wnoty.css" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+          <!-- notification css-->
+        <link href="../../assets/plugins/notify/wnoty.css" rel="stylesheet">
+
+        <script src="../../assets/plugins/jquery/jquery.min.js"></script>
+        <!-- style sheet -->
+        <link rel="stylesheet" href="../../css/admin_css/adminStyle.css">
+        <link rel="stylesheet" href="../../css/admin_css/affiliate.css">
+        <script type="text/javascript" src="https://sdk.monnify.com/plugin/monnify.js"></script>
+
+    </head>
+
+    <body>
+
+        <!-- Loader -->
+        <div id="gx-overlay">
+            <div class="gx-ellipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+
+        <div class="grid-container">
+            <!-- Header -->
+            <?php include('../../includes/affiliate-header.php'); ?>
+            <!--End Header -->
+
+
+            <!-- Sidebar -->
+            <?php include('../../includes/affiliate-menu.php'); ?>
+            <!-- End Sidebar -->
+
+
+            <!--======Floating Button=========-->
+            <!-- Buttons -->
+            <?php# include('../../includes/floating-btn.php'); ?>
+            <!-- End - Buttons -->
+            <!--======Floating Button=========-->
+
+
+            <!---====Side Modal Start Here====-->
+            <?php #include('../../includes/setting-menu.php'); ?>
+            <!---====Side Modal End Here====-->
+
+            <!----Main----->
+            <main class="main-container">
+
+                <div class="main-title">
+                    <span class="font-weight-bold">Transactions </span>
+                </div>
+
+                <!-- Content Row -->
+                <div class="row mt-4">
+
+                    <!-- Earnings (Monthly) Card Example -->
+                    <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="card border-left-primary shadow h-100 py-2" style="border-left: .25rem solid #36b9cc !important;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary mb-1">
+                                            Total Amount Credited</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800" id="f_l_aff_earn"></div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-naira-sign fa-2x text-gray-300">₦</i>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Earnings (Second level Affiliates) -->
+                    <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="card border-left-primary shadow h-100 py-2" style="border-left: .25rem solid #f6c23e !important;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary mb-1">
+                                            Total Amount Debited</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800" id="s_l_aff_earn"></div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-naira-sign fa-2x text-gray-300">₦</i>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-3 card p-3">
+
+                    <!-- Search and Filter Show -->
+                    <div class="row mt-2">
+
+                        <div class="col-xl-9 col-md-6 mb-1">
+                            <div class="input-group mb-3" style="width:65%;">
+                                <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+                                <input type="text" class="form-control form-control-sm" id="searchInput" placeholder="Search by Name, Ref. Number, Type, Amount..." aria-label="Search" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6 mb-1">
+                            <select class="form-select form-select-sm" id="aff_level" aria-label=".form-select-sm example">
+                                <option value="0" selected>Affiliate Level</option>
+                                <option value="1">Level One Affiliates</option>
+                                <option value="2">Level Two Affiliates</option>
+                            </select>
+                        </div>
+
+                    </div>
+
+                    <!-- Display All Affiliates -->
+                    <div class="row" id="display_affiliates">
+
+                    </div>
+                </div>
+
+            </main>
+
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.40.0/apexcharts.min.js"></script>
+        <script src="../../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- notification js -->
+        <script src="../../assets/plugins/notify/wnoty.js"></script>
+
+        <script src="../../js/admin_js/adminScript.js"></script>
+
+        <!-- header js -->
+        <?php include('../../controller/js/app/header.php'); ?>
+
+        <!-- affiliate js -->
+        <?php include('../../controller/js/affiliate/transactions.php'); ?>
+
+    </body>
+
+</html>
--- includes/affiliate-header.php
+++ includes/affiliate-header.php
@@ -1,16 +1,13 @@
 <?php
 
-        error_reporting(E_ALL);
-       ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
+    $pros_get_current_term = mysqli_query($link,"SELECT * FROM `session` INNER JOIN `termorsemester` WHERE `session`.`sessionStatus`='1' AND `termorsemester`.`status`=1");//PROS GET CURRENT TERM AND SESSION HERE
 
-       $pros_get_current_term = mysqli_query($link,"SELECT * FROM `session` INNER JOIN `termorsemester` WHERE `session`.`sessionStatus`='1' AND `termorsemester`.`status`=1");//PROS GET CURRENT TERM AND SESSION HERE
+    $pros_get_current_term_fetch = mysqli_fetch_assoc($pros_get_current_term);
 
-        $pros_get_current_term_fetch = mysqli_fetch_assoc($pros_get_current_term);
-
-       
-    
-    ?>
+?>
 <!-- Header -->
 <header class="header">
 
@@ -18,7 +15,7 @@
         <span class="material-icons-sharp">menu</span>
     </div>
 
-    <div class="header-left">
+    <div class="header-left d-flex gap-2">
         <div class="input-group">
 			<div class="input-group-text" style="border-radius:10px 0px 0px 10px;border-right:none;background:transparent;border-color:#007ffb;">
 				<i class='fas fa-calendar-alt sideicon' style="color:#007ffb;"></i>
@@ -27,45 +24,66 @@
             <input type="hidden" id="user_id" value="<?php echo $UserID; ?>">
             <input type="hidden" id="user_type" value="<?php echo $UType; ?>">
                 
-			<select class="form-select dropbtn abba-change-institution" id="autoSizingSelect" style="border-left:none;background:transparent;border-color:#007ffb;">
-				<option value="" selected disabled>Session</option>
-
+			<select class="form-select dropbtn abba-change-session" id="autoSizingSelect" style="border-left:none;background:transparent;border-color:#007ffb;">
+				<option value="0" selected>All Session</option>
 
                 <?php
 
-                $abba_sql_session = ("SELECT * FROM `session` ORDER BY sessionName DESC");
-                $abba_result_session = mysqli_query($link, $abba_sql_session);
-                $abba_row_session = mysqli_fetch_assoc($abba_result_session);
-                $abba_row_cnt_session = mysqli_num_rows($abba_result_session);
+                    $abba_sql_session = ("SELECT * FROM `session` ORDER BY sessionName DESC");
+                    $abba_result_session = mysqli_query($link, $abba_sql_session);
+                    $abba_row_session = mysqli_fetch_assoc($abba_result_session);
+                    $abba_row_cnt_session = mysqli_num_rows($abba_result_session);
 
-                if ($abba_row_cnt_session > 0) {
-                    do {
-                        if ($abba_row_session['sessionStatus'] == '1') {
-                            $abba_selected_session = 'selected';
-                        } else {
-                            $abba_selected_session = '';
-                        }
-                        echo '<option value="' . $abba_row_session['sessionName'] . '" ' . $abba_selected_session . '>' . $abba_row_session['sessionName'] . '</option>';
+                    if ($abba_row_cnt_session > 0) {
+                        do {
+                            if ($abba_row_session['sessionStatus'] == '1') {
+                                $abba_selected_session = 'selected';
+                            } else {
+                                $abba_selected_session = '';
+                            }
+                            echo '<option value="' . $abba_row_session['sessionName'] . '" ' . $abba_selected_session . '>' . $abba_row_session['sessionName'] . ' Session</option>';
 
-                    } while ($abba_row_session = mysqli_fetch_assoc($abba_result_session));
-                } else {
-                    echo '<option value="0">No Records Found</option>';
-                }
+                        } while ($abba_row_session = mysqli_fetch_assoc($abba_result_session));
+                    } else {
+                        echo '<option value="0">No Records Found</option>';
+                    }
                 ?>
 
-				
 			</select>
-			<!-- <div class="input-group-text dropdown-caret"
-				style="border-radius:0px 10px 10px 0px;border-left:none;background:transparent;border-color:#007ffb;">
-				<i class='fa fa-angle-double-down' style="color:#007ffb;"></i>
-			</div> -->
-		</div>
+
+            <input type="hidden" id="user_id" value="<?php echo $UserID; ?>">
+            <input type="hidden" id="user_type" value="<?php echo $UType; ?>">
+
+            <select class="form-select dropbtn abba-change-term" id="autoSizingSelect" style="border-left:none;background:transparent;border-color:#007ffb;">
+                <option value="0" selected>All Term</option>
+
+                <?php
+
+                    $abba_sql_termorsemester = ("SELECT * FROM `termorsemester`");
+                    $abba_result_termorsemester = mysqli_query($link, $abba_sql_termorsemester);
+                    $abba_row_termorsemester = mysqli_fetch_assoc($abba_result_termorsemester);
+                    $abba_row_cnt_termorsemester = mysqli_num_rows($abba_result_termorsemester);
+
+                    if ($abba_row_cnt_termorsemester > 0) {
+                        do {
+                            if ($abba_row_termorsemester['status'] == '1') {
+                                $abba_selected_termorsemester = 'selected';
+                            } else {
+                                $abba_selected_termorsemester = '';
+                            }
+                            echo '<option value="' . $abba_row_termorsemester['TermOrSemesterID'] . '" ' . $abba_selected_termorsemester . '>' . $abba_row_termorsemester['TermOrSemesterName'] . ' Term</option>';
+
+                        } while ($abba_row_termorsemester = mysqli_fetch_assoc($abba_result_termorsemester));
+                    } else {
+                        echo '<option value="0">No Records Found</option>';
+                    }
+                ?>
+
+            </select>
+        </div>
     </div>
 
 
-  
-
-    
     <div class="header-right">
 
         <span class="termCLA" style="margin-right: 50px;">
@@ -73,9 +91,6 @@
             <span><i class='bx bx-calendar'></i> <?php echo $pros_get_current_term_fetch['sessionName']?> / <?php echo $pros_get_current_term_fetch['TermOrSemesterName']?> Term</span>
         </span>
 
-     
-
-        
         <div class="btn-group" style="margin-top: -5px; ">
             <span type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
                 <?php
@@ -103,8 +118,3 @@
     </div>
 </header>
 <!--End Header -->
-
-
-
-
-
